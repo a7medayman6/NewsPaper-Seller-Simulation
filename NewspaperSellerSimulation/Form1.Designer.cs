bf4887c39b96f3d1bc9ed2fa4b2667fb3e5b0d28
Ahmed Ayman <a.ayman6000@gmail.com>
2021-12-19 17:17:33 +0200

    finished the simulation with success, please terminate the simulation objects

---
+++ NewspaperSellerSimulation/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace NewspaperSellerSimulation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.scrap_t = new System.Windows.Forms.TextBox();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label5 = new System.Windows.Forms.Label();
+            this.table_dgv = new System.Windows.Forms.DataGridView();
+            this.simulate_b = new System.Windows.Forms.Button();
+            this.selling_t = new System.Windows.Forms.TextBox();
+            this.purchace_t = new System.Windows.Forms.TextBox();
+            this.records_t = new System.Windows.Forms.TextBox();
+            this.newspaper_t = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.table_dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // scrap_t
+            // 
+            this.scrap_t.BackColor = System.Drawing.SystemColors.Control;
+            this.scrap_t.Location = new System.Drawing.Point(937, 345);
+            this.scrap_t.Margin = new System.Windows.Forms.Padding(2);
+            this.scrap_t.Name = "scrap_t";
+            this.scrap_t.Size = new System.Drawing.Size(43, 20);
+            this.scrap_t.TabIndex = 27;
+            // 
+            // Column10
+            // 
+            this.Column10.HeaderText = "daily net profit";
+            this.Column10.MinimumWidth = 8;
+            this.Column10.Name = "Column10";
+            this.Column10.Width = 150;
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "Scrap profit";
+            this.Column9.MinimumWidth = 8;
+            this.Column9.Name = "Column9";
+            this.Column9.Width = 150;
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "Lost profit";
+            this.Column8.MinimumWidth = 8;
+            this.Column8.Name = "Column8";
+            this.Column8.Width = 150;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Sales profit";
+            this.Column7.MinimumWidth = 8;
+            this.Column7.Name = "Column7";
+            this.Column7.Width = 150;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Daily cost";
+            this.Column6.MinimumWidth = 8;
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 150;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "demand";
+            this.Column5.MinimumWidth = 8;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 150;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Random demand";
+            this.Column4.MinimumWidth = 8;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 150;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "news day type";
+            this.Column3.MinimumWidth = 8;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Random news day type";
+            this.Column2.MinimumWidth = 8;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 150;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "DayNo";
+            this.Column1.MinimumWidth = 8;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(873, 347);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 13);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Scrap Price";
+            // 
+            // table_dgv
+            // 
+            this.table_dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table_dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column10});
+            this.table_dgv.Location = new System.Drawing.Point(13, 13);
+            this.table_dgv.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.table_dgv.Name = "table_dgv";
+            this.table_dgv.RowHeadersWidth = 62;
+            this.table_dgv.RowTemplate.Height = 24;
+            this.table_dgv.Size = new System.Drawing.Size(1045, 313);
+            this.table_dgv.TabIndex = 25;
+            // 
+            // simulate_b
+            // 
+            this.simulate_b.Location = new System.Drawing.Point(490, 412);
+            this.simulate_b.Name = "simulate_b";
+            this.simulate_b.Size = new System.Drawing.Size(100, 51);
+            this.simulate_b.TabIndex = 24;
+            this.simulate_b.Text = "Start Simulation";
+            this.simulate_b.UseVisualStyleBackColor = true;
+            this.simulate_b.Click += new System.EventHandler(this.simulate_b_Click);
+            // 
+            // selling_t
+            // 
+            this.selling_t.Location = new System.Drawing.Point(754, 345);
+            this.selling_t.Name = "selling_t";
+            this.selling_t.ReadOnly = true;
+            this.selling_t.Size = new System.Drawing.Size(43, 20);
+            this.selling_t.TabIndex = 23;
+            // 
+            // purchace_t
+            // 
+            this.purchace_t.Location = new System.Drawing.Point(578, 345);
+            this.purchace_t.Name = "purchace_t";
+            this.purchace_t.ReadOnly = true;
+            this.purchace_t.Size = new System.Drawing.Size(43, 20);
+            this.purchace_t.TabIndex = 22;
+            // 
+            // records_t
+            // 
+            this.records_t.Location = new System.Drawing.Point(376, 345);
+            this.records_t.Name = "records_t";
+            this.records_t.ReadOnly = true;
+            this.records_t.Size = new System.Drawing.Size(43, 20);
+            this.records_t.TabIndex = 21;
+            // 
+            // newspaper_t
+            // 
+            this.newspaper_t.Location = new System.Drawing.Point(152, 345);
+            this.newspaper_t.Name = "newspaper_t";
+            this.newspaper_t.ReadOnly = true;
+            this.newspaper_t.Size = new System.Drawing.Size(43, 20);
+            this.newspaper_t.TabIndex = 20;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 347);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Number of Newspapers";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(271, 347);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Number Of Records";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(680, 347);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Selling Price";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(488, 347);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Purchacing Price";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1070, 529);
+            this.Controls.Add(this.scrap_t);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.table_dgv);
+            this.Controls.Add(this.simulate_b);
+            this.Controls.Add(this.selling_t);
+            this.Controls.Add(this.purchace_t);
+            this.Controls.Add(this.records_t);
+            this.Controls.Add(this.newspaper_t);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.table_dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox scrap_t;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView table_dgv;
+        private System.Windows.Forms.Button simulate_b;
+        private System.Windows.Forms.TextBox selling_t;
+        private System.Windows.Forms.TextBox purchace_t;
+        private System.Windows.Forms.TextBox records_t;
+        private System.Windows.Forms.TextBox newspaper_t;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ NewspaperSellerSimulation/Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NewspaperSellerModels;
+using NewspaperSellerTesting;
+
+namespace NewspaperSellerSimulation
+{
+    public partial class Form1 : Form
+    {
+        SimulationSystem simulationSystem;
+        Random ran = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void simulate_b_Click(object sender, EventArgs e)
+        {
+            string[] data = File.ReadAllLines(Constants.FileNames.TestCase1);
+            simulationSystem =  readInput(data);
+            Simulate(simulationSystem);
+            Display(simulationSystem);
+
+            // TEST 
+            string testingResult = TestingManager.Test(simulationSystem, Constants.FileNames.TestCase1);
+            MessageBox.Show(testingResult);
+
+        }
+        private void Display(SimulationSystem simulationSystem)
+        {
+            simulationSystem.PerformanceMeasures.TotalSalesProfit = 0;
+            simulationSystem.PerformanceMeasures.TotalCost = 0;
+            simulationSystem.PerformanceMeasures.TotalLostProfit = 0;
+            simulationSystem.PerformanceMeasures.TotalScrapProfit = 0;
+            simulationSystem.PerformanceMeasures.TotalNetProfit = 0;
+            simulationSystem.PerformanceMeasures.DaysWithMoreDemand = 0;
+            simulationSystem.PerformanceMeasures.DaysWithUnsoldPapers = 0;
+            for (int i = 0; i < simulationSystem.SimulationTable.Count; i++)
+            {
+                table_dgv.Rows.Add
+                    (
+                       simulationSystem.SimulationTable[i].DayNo, 
+                       simulationSystem.SimulationTable[i].NewsDayType,
+                       simulationSystem.SimulationTable[i].RandomNewsDayType, 
+                       simulationSystem.SimulationTable[i].RandomDemand,
+                       simulationSystem.SimulationTable[i].Demand, 
+                       simulationSystem.SimulationTable[i].DailyCost, 
+                       simulationSystem.SimulationTable[i].SalesProfit, 
+                       simulationSystem.SimulationTable[i].LostProfit,
+                       simulationSystem.SimulationTable[i].ScrapProfit, 
+                       simulationSystem.SimulationTable[i].DailyNetProfit
+                    );
+
+                simulationSystem.PerformanceMeasures.TotalSalesProfit += simulationSystem.SimulationTable[i].SalesProfit;
+                simulationSystem.PerformanceMeasures.TotalCost += simulationSystem.SimulationTable[i].DailyCost;
+                simulationSystem.PerformanceMeasures.TotalLostProfit += simulationSystem.SimulationTable[i].LostProfit;
+                simulationSystem.PerformanceMeasures.TotalScrapProfit += simulationSystem.SimulationTable[i].ScrapProfit;
+                simulationSystem.PerformanceMeasures.TotalNetProfit += simulationSystem.SimulationTable[i].DailyNetProfit;
+                
+                if (simulationSystem.SimulationTable[i].Demand > simulationSystem.NumOfNewspapers)
+                    simulationSystem.PerformanceMeasures.DaysWithMoreDemand++;
+                if (simulationSystem.SimulationTable[i].Demand < simulationSystem.NumOfNewspapers)
+                    simulationSystem.PerformanceMeasures.DaysWithUnsoldPapers++;
+            }
+        }
+        private void Simulate(SimulationSystem simulationSystem)
+        {
+            for (int Record = 0; Record < simulationSystem.NumOfRecords; Record++)
+                simulationSystem.SimulationTable.Add(SimulateAcase(simulationSystem, Record));
+        }
+        private SimulationCase SimulateAcase(SimulationSystem simulationSystem, int Record)
+        {
+            SimulationCase simulationCase = new SimulationCase();
+            
+            int RandomDayType = GenerateRandomInt();
+            int RandomDemand = GenerateRandomInt();
+
+            int DayType = -1, Demand = -1;
+
+            if (RandomDayType <= simulationSystem.DayTypeDistributions[0].MaxRange)
+            {
+                DayType = 0;
+                simulationCase.NewsDayType = Enums.DayType.Good;
+            }
+            else if (RandomDayType <= simulationSystem.DayTypeDistributions[1].MaxRange)
+            {
+                DayType = 1;
+                simulationCase.NewsDayType = Enums.DayType.Fair;
+            }
+            else
+            {
+                DayType = 2;
+                simulationCase.NewsDayType = Enums.DayType.Poor;
+            }
+
+            for (int i = 0; i < simulationSystem.DemandDistributions.Count; i++)
+                if (RandomDemand >= simulationSystem.DemandDistributions[i].DayTypeDistributions[DayType].MinRange &&
+                    RandomDemand <= simulationSystem.DemandDistributions[i].DayTypeDistributions[DayType].MaxRange)
+                {
+                    
+                    Demand = simulationSystem.DemandDistributions[i].Demand;
+                    break;
+                }
+
+            simulationCase.DailyCost = CalcNewspapersCost(simulationSystem.PurchasePrice, simulationSystem.NumOfNewspapers);
+            simulationCase.SalesProfit = CalcSalesRevnue(Demand, simulationSystem.SellingPrice, simulationSystem.NumOfNewspapers);
+            simulationCase.LostProfit = CalcLostProfit(Demand, simulationSystem.SellingPrice, simulationSystem.PurchasePrice, simulationSystem.NumOfNewspapers);
+            simulationCase.ScrapProfit = CalcScrapProfit(Demand, simulationSystem.ScrapPrice, simulationSystem.NumOfNewspapers);
+            simulationCase.DailyNetProfit = CalcTotalProfit(Demand, simulationSystem.SellingPrice, simulationSystem.PurchasePrice, simulationSystem.ScrapPrice, simulationSystem.NumOfNewspapers);
+            simulationCase.DayNo = Record + 1;
+            simulationCase.RandomNewsDayType = RandomDayType;
+            simulationCase.RandomDemand = RandomDemand;
+            simulationCase.Demand = Demand;
+
+            return simulationCase;
+        }
+        private decimal CalcSalesRevnue(int Demand, decimal SellingPrice, int N)
+        {
+            if (N > Demand)
+                return (decimal) Demand * SellingPrice;
+            return (decimal) N * SellingPrice;
+        }
+        decimal CalcLostProfit(int Demand, decimal SellingPrice, decimal PurchasePrice, int N)
+        {
+            if (Demand > N)
+                return ((Demand * SellingPrice) - (Demand * PurchasePrice)) - ((N * SellingPrice) - (N * PurchasePrice));
+            return 0;
+        }
+        private decimal CalcScrapProfit(int Demand, decimal ScrapPrice, int N)
+        {
+            if (Demand < N)
+                return (N - Demand) * ScrapPrice;
+            return 0;
+        }
+        private decimal CalcNewspapersCost(decimal PurchasePrice, int N)
+        {
+            return N * PurchasePrice;
+        }
+        private decimal CalcTotalProfit(int Demand, decimal SellingPrice, decimal PurchasePrice, decimal ScrapPrice, int N)
+        {
+            return CalcSalesRevnue(Demand, SellingPrice, N) - CalcNewspapersCost(PurchasePrice, N) - CalcLostProfit(Demand, SellingPrice, PurchasePrice, N) + CalcScrapProfit(Demand, ScrapPrice, N);
+        }
+        private SimulationSystem readInput(string[] data)
+        {
+            simulationSystem = new SimulationSystem();
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] == "NumOfNewspapers")
+                {
+                    newspaper_t.Text = data[i + 1];
+                    simulationSystem.NumOfNewspapers = int.Parse(data[i + 1]);
+                }
+                else if (data[i] == "NumOfRecords")
+                {
+                    records_t.Text = data[i + 1];
+                    simulationSystem.NumOfRecords = int.Parse(data[i + 1]);
+                }
+                else if (data[i] == "PurchasePrice")
+                {
+                    purchace_t.Text = data[i + 1];
+                    simulationSystem.PurchasePrice = decimal.Parse(data[i + 1]);
+                }
+                else if (data[i] == "SellingPrice")
+                {
+                    selling_t.Text = data[i + 1];
+                    simulationSystem.SellingPrice = decimal.Parse(data[i + 1]);
+                }
+                else if (data[i] == "ScrapPrice")
+                {
+                    scrap_t.Text = data[i + 1];
+                    simulationSystem.ScrapPrice = decimal.Parse(data[i + 1]);
+                }
+                else if (data[i] == "DayTypeDistributions")
+                {
+                    // advance to the next line
+                    i++;
+                        
+                    // get the next line values
+                    string[] DayTypeDists = data[i].Split(',');
+
+                    // get the day type dists
+                    simulationSystem.DayTypeDistributions = CalcDayTypeDistribution(DayTypeDists);
+                    
+                }
+                else if (data[i] == "DemandDistributions")
+                {
+                    i++;
+                    while (i < data.Length && data[i] != "")
+                    {
+                        DemandDistribution DemandDistribution = new DemandDistribution();
+                        string[] DemandDists = data[i].Split(',');
+
+                        DemandDistribution.Demand = int.Parse(DemandDists[0]);
+
+                        List<DayTypeDistribution> DayTypeDistribution = new List<DayTypeDistribution>();
+                        DayTypeDistribution DayTypeDistSpec;
+
+                        for (int j = 1; j <= 3; j++)
+                        {
+                            DayTypeDistSpec = new DayTypeDistribution();
+                            DayTypeDistSpec.Probability = decimal.Parse(DemandDists[j]);
+                            if (j == 1)
+                                DayTypeDistSpec.DayType = Enums.DayType.Good;
+                            else if (j == 2)
+                                DayTypeDistSpec.DayType = Enums.DayType.Fair;
+                            else
+                                DayTypeDistSpec.DayType = Enums.DayType.Poor;
+
+                            DayTypeDistribution.Add(DayTypeDistSpec);
+                        }
+                        DemandDistribution.DayTypeDistributions = DayTypeDistribution;
+                        simulationSystem.DemandDistributions.Add(DemandDistribution);
+                        i++;
+
+                    }
+                    for (int DemCounter = 0; DemCounter < simulationSystem.DemandDistributions.Count; DemCounter++)
+                    {
+                        for (int DayTypeCounter = 0; DayTypeCounter < 3; DayTypeCounter++)
+                        {
+                            if (DemCounter == 0)
+                            {
+                                simulationSystem.DemandDistributions[DemCounter].DayTypeDistributions[DayTypeCounter].CummProbability = simulationSystem.DemandDistributions[DemCounter].DayTypeDistributions[DayTypeCounter].Probability;
+                                simulationSystem.DemandDistributions[DemCounter].DayTypeDistributions[DayTypeCounter].MinRange = 0;
+                                simulationSystem.DemandDistributions[DemCounter].DayTypeDistributions[DayTypeCounter].MaxRange = (int)(simulationSystem.DemandDistributions[DemCounter].DayTypeDistributions[DayTypeCounter].CummProbability * 100);
+                            }
+                            else
+                            {
+                                simulationSystem.DemandDistributions[DemCounter].DayTypeDistributions[DayTypeCounter].MinRange = simulationSystem.DemandDistributions[DemCounter - 1].DayTypeDistributions[DayTypeCounter].MaxRange + 1;
+                                simulationSystem.DemandDistributions[DemCounter].DayTypeDistributions[DayTypeCounter].CummProbability = simulationSystem.DemandDistributions[DemCounter].DayTypeDistributions[DayTypeCounter].Probability + simulationSystem.DemandDistributions[DemCounter - 1].DayTypeDistributions[DayTypeCounter].CummProbability;
+                                simulationSystem.DemandDistributions[DemCounter].DayTypeDistributions[DayTypeCounter].MaxRange = (int)(simulationSystem.DemandDistributions[DemCounter].DayTypeDistributions[DayTypeCounter].CummProbability * 100); 
+                            }
+                        }
+                    }
+                }
+            }
+
+            return simulationSystem;
+            
+        }
+        private List<DayTypeDistribution> CalcDayTypeDistribution(string[] DayTypeDists)
+        {
+            DayTypeDistribution DT;
+            List<DayTypeDistribution> DTList = new List<DayTypeDistribution>();
+
+            for (int j = 0; j < DayTypeDists.Length; j++)
+            {
+                string typeDist = DayTypeDists[j];
+
+                DT = new DayTypeDistribution();
+
+                // read the probability of type j
+                DT.Probability = decimal.Parse(typeDist);
+
+                // calc the cumm prob
+                DT.CummProbability = DT.Probability;
+
+                if(j != 0)
+                    DT.CummProbability += DTList[DTList.Count - 1].CummProbability;
+
+                // (if not first type) calc the min range -> prev type max range
+                DT.MinRange = j == 0 ? 1 : DTList[DTList.Count - 1].MaxRange + 1;
+
+                // (if not last type) calc the max range -> prob * 100
+                DT.MaxRange = j == DayTypeDists.Length - 1 ? 100 : Decimal.ToInt32(DT.CummProbability * 100);
+
+                if (j == 0)
+                    DT.DayType = Enums.DayType.Good;
+                else if (j == 1)
+                    DT.DayType = Enums.DayType.Fair;
+                else
+                    DT.DayType = Enums.DayType.Poor;
+
+                DTList.Add(DT);
+            }
+
+            return DTList;
+        }
+
+        public int GenerateRandomInt()
+        {
+            int number = ran.Next(1, 101);
+            return number;
+        }
+    }
+}
